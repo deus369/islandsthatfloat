fffebe93dc90c749d7dc00a396d0914046ef7715
Marz <marz.tierney@gmail.com>
2022-10-22 21:11:49 +1100

    Implemented volume slider. Thank you google.

--- project/Assets/Manthan/Scripts/MainMenu.cs
+++ project/Assets/Manthan/Scripts/MainMenu.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
     void Awake()
     {
         Cursor.visible = true;
@@ -21,5 +23,20 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("QUIT");   
         Application.Quit();
+    }
+
+    public void SetVolume(float value)
+    {  
+        float newVolume = 0f;
+        if (value != 0)
+        {
+            newVolume = Mathf.Log10(value) * 20 + 2;
+        }
+        else
+        {
+            newVolume = -80f;
+        }
+        audioMixer.SetFloat("VolumeMusic", newVolume);
+        // Debug.Log("Set music volume to: " + newVolume);
     }
 }
